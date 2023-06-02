df1d120f3a149bc6a9cbda4621749ca2a0b85a6f
kumastry <chma19063@g.nihon-u.ac.jp>
2023-06-02 13:13:22 +0900

    ansを算出

--- gradient_descent/src/main.rs
+++ gradient_descent/src/main.rs
@@ -6,21 +6,28 @@ fn main() {
                           0.0, -1.0, 3.0);
     let b = na::Vector3::new(100.0, 50.0, 50.0);
 
-    let xk = na::Vector3::new(1.0, 2.0, 3.0);
+    let mut xk = na::Vector3::new(1.0, 2.0, 3.0);
 
-    let grad_xk = a * xk + b;
+    let mut grad_xk = a * xk + b;
     let gg:f64 = grad_xk.norm();
 
     const EPS:f64 = 0.1;
     //let dk:Vector3<f64> = - grad_xk.clone();
     println!("{:?}", grad_xk);
     println!("{}", gg);
-    println!("{:?}", dk);
     println!("{:?}", grad_xk);
 
-    while grad_xk.norm() < 0.1 {
-        let dk:Vector3<f64> = - grad_xk.clone();
-        let ak:f64 = 0.1
+    println!("start");
+    println!("{:?}", xk);
+    while grad_xk.norm() > EPS {
 
+        let dk:Vector3<f64> = - grad_xk.clone();
+        let ak:f64 = 0.1;
+
+        xk = xk + ak * dk;
+        grad_xk = a * xk + b;
     }
+
+    println!("ans");
+    println!("{:?}", xk);
 }
